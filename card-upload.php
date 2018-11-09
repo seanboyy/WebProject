338d2b67b66a702e8723ec7cb5eb497915e2791f
wytriam <thomas.m.frick@gmail.com>
2018-11-08 23:01:35 -0500

    finished card upload page and added script

---
+++ card-upload.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Magic Maker - Card Uploading</title>
+	</head>
+	<body>
+		<p>This feature is not yet implemented!</p>
+	</body>
+</html>
