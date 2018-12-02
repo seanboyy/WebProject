72fef8b8046293ad3d4b8f829a9e91884d5657a6
wytriam <thomas.m.frick@gmail.com>
2018-12-01 21:06:37 -0500

    Custom Card now navigate to card-view when clicked on; card-view works (though it's not the prettiest)

--- get-images.php
+++ get-images.php
@@ -8,10 +8,10 @@
 				echo("failed to connect!");
 			}
 			else{
-				$result = $conn->query("SELECT `card_image` FROM `custom_cards`");
+				$result = $conn->query("SELECT card_id, card_image FROM `custom_cards` ");
 				$reversed = array_reverse($result->fetch_all());
 				for($i = 0; $i < count($reversed); ++$i){
-					echo "<img alt='".$reversed[$i][0]."' src='".$reversed[$i][0]."' class='cardFormat'/>";
+					echo "<a href= \"./card-view.php?id=" . $reversed[$i][0] . "\"><img alt='".$reversed[$i][0]."' src='".$reversed[$i][1]."' class='cardFormat'/></a>";
 				}
 			}
 		?>
