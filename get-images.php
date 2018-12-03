2b6b936b3ce3b9cb7673200e2256d7caffa9e2af
wytriam <thomas.m.frick@gmail.com>
2018-12-02 20:50:48 -0500

    Fixed brokenness

--- get-images.php
+++ get-images.php
@@ -11,7 +11,7 @@
 				$result = $conn->query("SELECT card_id, card_image FROM `custom_cards` ");
 				$reversed = array_reverse($result->fetch_all());
 				for($i = 0; $i < count($reversed); ++$i){
-					echo "<span onmouseenter='doEntry(".$reversed[$i][0].", this)' onmouseleave='doLeave()'><img alt='".$reversed[$i][0]."' src='".$reversed[$i][1]."' class='cardFormat'/></span>";
+					echo "<span onmouseenter='doEntry(".$reversed[$i][0].", this)' onmouseleave='doLeave()'><a href='./card-view.php?id=".$reversed[$i][0]."><img alt='".$reversed[$i][0]."' src='".$reversed[$i][1]."' class='cardFormat'/></a></span>";
 				}
 			}
 		?>
