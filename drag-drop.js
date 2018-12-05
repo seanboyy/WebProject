9ad44027b5f3dbd880cc5a4c6b6ae2cb5a17b3e9
GCC\MCKINNISJP2 <mckinnisjp2@gcc.edu>
2018-12-05 11:35:59 -0500

    No big changes

--- drag-drop.js
+++ drag-drop.js
@@ -10,5 +10,19 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.currentTarget.appendChild(document.getElementById(data));
+	//$("#deck-images").append("<input type='button' id='removebtn'>");
 	$('#f-deck-list').append(data + " ");
 }
+/*
+function removeCard(){
+	console.log("removing...");
+	console.log($(this));
+	//var imagesrc =  $(this).prev().attr('src');
+	$(this).remove();
+	//console.log(imagesrc);
+	//var strArr = $('#f-deck-list').val().split(" ");
+	//console.log(strArr.filter(e => e !== imagesrc));
+	//strArr = strArr.filter(e => e !== imagesrc);
+	//$('#f-deck-list').val() = strArr;
+}
+*/
